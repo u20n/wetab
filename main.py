fc156a2a575b4c7f1ff22925f3796ec3559afc27
equi <git@notu.dev>
2023-02-10 16:53:04 -0500

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord, sys
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+is_connected = lambda guild : discord.utils.get(bot.voice_clients, guild=guild)
+
+@bot.command()
+async def join(ctx):
+    if is_connected(ctx.guild) == None: 
+        vc = await ctx.message.author.voice.channel.connect()
+        await ctx.send(f"**+**{ctx.message.author.voice.channel}")
+        vc.play(FFmpegPCMAudio("https://playerservices.streamtheworld.com/api/livestream-redirect/WETAFM.mp3"))
+
+@bot.command()
+async def leave(ctx):
+    if is_connected(ctx.guild): 
+        await ctx.send(f"**~**{ctx.message.author.voice.channel}")
+        await ctx.guild.voice_client.disconnect()
+
+bot.run(sys.argv[1])
