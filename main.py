b124d4987c3cee12ac3db99df174db9430987361
equi <git@notu.dev>
2023-02-10 18:51:03 -0500

    Small updates
    - Add VC intent
    - Stop audio before leaving channel

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.voice_states = True
 
 bot = commands.Bot(command_prefix='>', intents=intents)
 
@@ -13,13 +14,14 @@ is_connected = lambda guild : discord.utils.get(bot.voice_clients, guild=guild)
 async def join(ctx):
     if is_connected(ctx.guild) == None: 
         vc = await ctx.message.author.voice.channel.connect()
-        await ctx.send(f"**+**{ctx.message.author.voice.channel}")
+        await ctx.send(f"+{ctx.message.author.voice.channel}")
         vc.play(FFmpegPCMAudio("https://playerservices.streamtheworld.com/api/livestream-redirect/WETAFM.mp3"))
 
 @bot.command()
 async def leave(ctx):
     if is_connected(ctx.guild): 
-        await ctx.send(f"**~**{ctx.message.author.voice.channel}")
+        await ctx.send(f"-{ctx.message.author.voice.channel}")
+        ctx.guild.voice_client.stop()
         await ctx.guild.voice_client.disconnect()
 
 bot.run(sys.argv[1])
